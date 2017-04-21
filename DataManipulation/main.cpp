dd722e74a071df6e7910ef723f449b5f2e9fbe40
Kevin Baker <kbaker88@users.noreply.github.com>
2017-04-20 21:46:44 -0700

    void pointers and virtual alloc

--- DataManipulation/main.cpp
+++ DataManipulation/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <Windows.h>
 #include "sort.h"
 #include "datastructures.h" 
 #include "algorithms.h"
@@ -8,8 +9,64 @@
 
 using namespace std;
 
+#define KB 1024
+#define MB (KB * KB)
+#define GB (MB * KB)
+
+struct Test
+{
+	int x, y, z;
+	char a;
+};
+
+void* Memory = 0;
+void* MemPtr = Memory;
+
+char* AllocateCharArr(unsigned int Size)
+{
+	char *Start;
+	char *End;
+	Start = (char*)MemPtr;
+	End = Start + Size;
+	*End = '>';
+	return Start;
+}
+
 int main()
 {
+	_SYSTEM_INFO SystemInfo;
+
+	GetSystemInfo(&SystemInfo);
+	unsigned int NumberOfPages = GB / SystemInfo.dwPageSize;
+
+	//void* Memory = 0;
+	//void* MemPtr = Memory;
+
+	Memory = VirtualAlloc(0, NumberOfPages * SystemInfo.dwPageSize, MEM_COMMIT, PAGE_READWRITE);
+
+	Test *Testing = (Test*)Memory;
+	Testing->x = 10;
+	Testing->y = 1024;
+	Testing->z = 2;
+	Testing->a = 'a';
+	MemPtr = (char*)Memory + sizeof(Test);
+
+	Test *Testing2 = (Test*)MemPtr;
+	Testing2->a = 'c';
+	Testing2->x = 2;
+	Testing2->y = 8;
+	Testing2->z = 16;
+	MemPtr = (char*)MemPtr + sizeof(Test);
+
+	char* CharTest = AllocateCharArr(12);
+	CharTest[0] = 'a';
+	CharTest[1] = 'b';
+	CharTest[2] = 'c';
+	CharTest[3] = 'd';
+	CharTest[11] = 'z';
+
+	VirtualFree(Memory, NumberOfPages * SystemInfo.dwPageSize, 
+		MEM_RELEASE);
 
 	char Buffer[16] = {};
 	float Float = 0.0f;
@@ -32,9 +89,9 @@ int main()
 	}
 	cout << endl << endl;
 
-
-	int IntArray[20] = { 8, 23, 9, 1, 85, 3465, 3, 9, 0, 12, 13,
-	53, 5, 7, 6, 743, 43, 23, 85, -1 };
+	int IntArray[20] = { 8, 23, 9, 1, 85, 
+		3465, 3, 9, 0, 12, 13, 53, 5, 7,
+		6, 743, 43, 23, 85, -1 };
 
 	//QuickSort(IntArray, 0, 19);
 	MergeSort(IntArray, 20);
@@ -45,15 +102,17 @@ int main()
 	}
 	cout << endl;
 
-	int graph[MaxVNumber][MaxVNumber] = { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-	{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-	{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-	{ 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-	{ 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-	{ 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-	{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-	{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-	{ 0, 0, 2, 0, 0, 0, 6, 7, 0 }
+	int graph[MaxVNumber][MaxVNumber] = 
+	{ 
+	{ 0,  4,  0,  0,  0,  0,  0,  8,  0 },
+	{ 4,  0,  8,  0,  0,  0,  0, 11,  0 },
+	{ 0,  8,  0,  7,  0,  4,  0,  0,  2 },
+	{ 0,  0,  7,  0,  9, 14,  0,  0,  0 },
+	{ 0,  0,  0,  9,  0, 10,  0,  0,  0 },
+	{ 0,  0,  4, 14, 10,  0,  2,  0,  0 }, 
+	{ 0,  0,  0,  0,  0,  2,  0,  1,  6 },
+	{ 8, 11,  0,  0,  0,  0,  1,  0,  7 },
+	{ 0,  0,  2,  0,  0,  0,  6,  7,  0 }
 	};
 
 	int Result[9] = {};
